e8f439c2c8158aed5d914534f08b60aa80c78c36
exe <almaznon1@gmail.com>
2024-06-28 11:04:20 +0300

    'v.2.1'

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 DATA = {
@@ -19,12 +20,45 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipes_views(request, recipe:str):
+    count_person = int(request.GET.get('person', 1))
+    dict_person = {}
+    context = {}
+
+    data = DATA.get(recipe)
+    for name, ingr in data.items():
+        dict_person[name] = ingr * count_person
+        context.setdefault('recipe', dict_person)
+
+
+
+    return render(request, 'calculator/index.html', context )
+
+
+
+
+
+
+
+
+
+
+
+
+    # count_person = int(request.GET.get('person', 1))
+    # dict_person = {}
+    # final_dish = {}
+    # data = DATA.get(recipe)
+    # for name, dish in data.items():
+    #     dict_person[name] = dish * count_person
+    # final_dish.setdefault('recipe', dict_person)
+    #
+    #
+    #
+    #
+    # return render(request, 'calculator/index.html', final_dish)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
 
+
+
+from calculator.views import recipes_views, DATA
+
+# from recipes.calculator.views import rec
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('<recipe>/', recipes_views),
 ]
